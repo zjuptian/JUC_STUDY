08a6c657d1797e0c51da21b614a08de13d940046
zjuptian <85244795@qq.com>
2021-09-13 20:53:03 +0800

    test: thread join

---
+++ src/main/java/com/tp/juc/JoinTest.java
@@ -0,0 +1,36 @@
+package com.tp.juc;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created on 2021/9/13
+ *
+ * @author Patric Tian
+ */
+@Slf4j
+public class JoinTest {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t2 = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.info("2");
+        }, "t2");
+
+        Thread t1 = new Thread(() -> {
+            //t2 insert
+            try {
+                t2.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.info("1");
+        }, "t1");
+
+        t1.start();
+        t2.start();
+
+    }
+}
